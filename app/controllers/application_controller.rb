dd80ee06bc24eca558bc4a42a0590aa58e96fc39
HungNH <nguyen.huy.hungb@framgia.com>
2016-10-28 15:47:21 +0700

    [Admin] Add Category

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+  private
+  def check_logged
+    unless logged_in?
+      flash[:danger] = t "not_login"
+      redirect_to root_path
+    end
+  end
+
+  def check_admin
+    unless current_user.is_admin?
+      flash[:danger] = t "access_denied"
+      redirect_to root_path
+    end
+  end
 end
