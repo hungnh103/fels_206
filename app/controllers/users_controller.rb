dc514c85e18fab8fc97867d755bfcc0e48b7bdaf
HungNH <nguyen.huy.hungb@framgia.com>
2016-11-01 14:49:18 +0700

    [User] List users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+  before_action :check_logged, except: [:new, :create]
   before_action :load_user, only: [:show, :edit, :update]
+
+  def index
+    @users = User.select(:id, :name, :email).order(created_at: :DESC)
+      .paginate page: params[:page], per_page: Settings.user.per_page
+  end
 
   def show
   end
